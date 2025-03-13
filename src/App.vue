974ee728f74420143280baf25cab2ba969862b86
Thong17 <chhanbunthong@gmail.com>
2025-03-13 22:19:29 +0700

    update: add prettier format

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// hello
+  // hello
 </script>
 <template key>
   <router-view></router-view>
@@ -12,5 +12,4 @@
   <a href="/test" key="test" ref="test" target="_blank" rel="noopener">Hello</a>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
