db118a0a11f3c439f33b4d081b9e0b3a3ba54d24
Thong17 <chhanbunthong@gmail.com>
2025-03-15 21:48:06 +0700

    update: add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,11 @@
 <script setup lang="ts">
+  import MainLayout from './components/MainLayout.vue'
   // Hello
 </script>
 <template>
-  <router-view />
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
-  <a
-    key="test"
-    ref="test"
-    href="/test"
-    target="_blank"
-    rel="noopener"
-  >Hello</a>
+  <MainLayout title="Layout Title">
+    <h1>Hello</h1>
+  </MainLayout>
 </template>
 
 <style scoped></style>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>{{ title }}</header>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'MainLayout',
+    props: {
+      title: {
+        type: String,
+        default: 'Main Layout',
+      },
+    },
+  }
+</script>
+
+<style></style>
