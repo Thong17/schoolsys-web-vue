f731fcbe304471d049b2cb6894eb5663e7298410
Thong17 <chhanbunthong@gmail.com>
2025-03-27 09:12:26 +0700

    fix: format

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-	],
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
